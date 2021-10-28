901f2f5b8049b24999baea5f20b20b783b8040d1
codetobuild <goodnakha@gmail.com>
2021-10-28 14:11:59 +0530

    update

--- loader/db.js
+++ loader/db.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 
 const startDb = () => {
-  let url = `mongodb://localhost:27017/task-Manager`;
+  let dburl = `mongodb://localhost:27017/task-Manager`;
   if (process.env.NODE_ENV == "production") {
-    url = process.env.DB_URL;
+    dburl = process.env.DB_URL;
   }
-  mongoose.connect("mongodb://localhost:27017/task-Manager", {
+  mongoose.connect(dburl, {
     useNewUrlParser: true,
   });
 
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error: "));
   db.once("open", function () {
-    console.log("DB connected"); 
+    console.log("DB connected");
   });
 };
 
 module.exports = startDb;
-
